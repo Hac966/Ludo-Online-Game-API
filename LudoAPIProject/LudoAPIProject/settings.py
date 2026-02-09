c64071f4d675b4c7a405f8d31e66bd21bb9cd0e6
Fezile Mazibuko <82752172+Hac966@users.noreply.github.com>
2026-02-09 20:00:28 +0200

    Update settings.py

--- LudoAPIProject/LudoAPIProject/settings.py
+++ LudoAPIProject/LudoAPIProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--)b2tj30&5h_%cxr!+ei7manu5p(47r^dfa78ms=x6@h*qqmd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
